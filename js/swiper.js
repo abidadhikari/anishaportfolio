462df5ebaef3696928c3df13c997cd4a218db549
Abid Adhikari <abidadhikari@gmail.com>
2022-02-02 19:54:36 +0545

    skills and education , swipper added

---
+++ js/swiper.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
